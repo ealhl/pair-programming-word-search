f494d56cf7090fcc43b08f245d797cba6d4bff38
ealhl <yayt.tuk@gmail.com>
2023-12-17 14:54:20 -0800

    update wordssearch

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,29 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  if (letters.length === 0 || word.length === 0) {
+    return false;
+  }
 
-module.exports = wordSearch
+  //check for vertical words
+  const verticalJoin = [];
+  for (let i = 0; i < letters[0].length; i++) {
+    let verticalWord = "";
+    for (let j = 0; j < letters.length; j++) {
+      verticalWord += letters[j][i];
+    }
+    verticalJoin.push(verticalWord);
+    // console.log(verticalWord);
+  }
+  for (let l of verticalJoin) {
+    if (l.includes(word)) return true;
+  }
+
+  //check for horizontal words
+  const horizontalJoin = letters.map((ls) => ls.join(""));
+  for (let l of horizontalJoin) {
+    // console.log(l);
+    if (l.includes(word)) return true;
+  }
+  return false;
+};
+
+module.exports = wordSearch;
